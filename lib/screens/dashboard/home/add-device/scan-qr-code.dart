7360029d36adc886a1b7a4781e22b8592b692668
tade-dev <akintadeseun816@gmail.com>
2024-08-04 18:50:45 +0100

    updated

--- lib/screens/dashboard/home/add-device/scan-qr-code.dart
+++ lib/screens/dashboard/home/add-device/scan-qr-code.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:vescan/routes/app/app-route-names.dart';
 
 class ScanQRCodeScreen extends StatefulWidget {
@@ -16,8 +16,8 @@ class ScanQRCodeScreen extends StatefulWidget {
 }
 
 class _ScanQRCodeScreenState extends State<ScanQRCodeScreen> {
-  Barcode? result;
-  QRViewController? controller;
+  // Barcode? result;
+  // QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
   // @override
@@ -91,11 +91,13 @@ class _ScanQRCodeScreenState extends State<ScanQRCodeScreen> {
                       const SizedBox(
                         height: 20,
                       ),
-                      SizedBox(
-                        height: 417,
-                        width: double.infinity,
-                        child: _buildQrView(context),
-                      ),
+                      // SizedBox(
+                      //   height: 417,
+                      //   width: double.infinity,
+                      //   child: 
+                      //   _buildQrView(context),
+                      // ),
+                      Image.asset("assets/images/qr-code.png"),
                       const SizedBox(
                         height: 40,
                       ),
@@ -144,50 +146,50 @@ class _ScanQRCodeScreenState extends State<ScanQRCodeScreen> {
     );
   }
 
-  Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    var scanArea = (MediaQuery.of(context).size.width < 400 ||
-            MediaQuery.of(context).size.height < 400)
-        ? 150.0
-        : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 10,
-          cutOutSize: scanArea),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-    );
-  }
+  // Widget _buildQrView(BuildContext context) {
+  //   // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+  //   var scanArea = (MediaQuery.of(context).size.width < 400 ||
+  //           MediaQuery.of(context).size.height < 400)
+  //       ? 150.0
+  //       : 300.0;
+  //   // To ensure the Scanner view is properly sizes after rotation
+  //   // we need to listen for Flutter SizeChanged notification and update controller
+  //   return QRView(
+  //     key: qrKey,
+  //     onQRViewCreated: _onQRViewCreated,
+  //     overlay: QrScannerOverlayShape(
+  //         borderColor: Colors.red,
+  //         borderRadius: 10,
+  //         borderLength: 30,
+  //         borderWidth: 10,
+  //         cutOutSize: scanArea),
+  //     onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+  //   );
+  // }
 
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
+  // void _onQRViewCreated(QRViewController controller) {
+  //   setState(() {
+  //     this.controller = controller;
+  //   });
+  //   controller.scannedDataStream.listen((scanData) {
+  //     setState(() {
+  //       result = scanData;
+  //     });
+  //   });
+  // }
 
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
-      );
-    }
-  }
+  // void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+  //   log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+  //   if (!p) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text('no Permission')),
+  //     );
+  //   }
+  // }
 
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   controller?.dispose();
+  //   super.dispose();
+  // }
 }
--- lib/widgets/bottom-sheets/inspection-fee-sheet.dart
+++ lib/widgets/bottom-sheets/inspection-fee-sheet.dart
@@ -1,99 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:iconsax/iconsax.dart';
 
 class InspectionFeeSheet {
   show() {
     Get.bottomSheet(
-      Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-          )
-        ),
-        child: Column(
-          children: [
-            const Text(
-              "Inspection Fee",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontFamily: "OpenBold"
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              )),
+          child: Column(
+            children: [
+              const Text(
+                "Inspection Fee",
+                style: TextStyle(
+                    color: Colors.black, fontSize: 20, fontFamily: "OpenBold"),
               ),
-            ),
-            const SizedBox(height: 20,),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: const Color(0xffE2E2E4)
-                )
+              const SizedBox(
+                height: 20,
               ),
-              child: const Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Inspection Fee",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontFamily: "OpenBold"
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xffE2E2E4))),
+                child: const Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Inspection Fee",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontFamily: "OpenBold"),
                           ),
-                        ),
-                        const Text(
-                          "N 5,000",
-                          style: TextStyle(
-                            color: Color(0xff00BFFF),
-                            fontSize: 12,
+                          const Text(
+                            "N 5,000",
+                            style: TextStyle(
+                              color: Color(0xff00BFFF),
+                              fontSize: 12,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Divider(
-                    thickness: 1,
-                    color: Color(0xffE2E2E4),
-                  ),
-                  Divider(
-                    thickness: 1,
-                    color: Color(0xffE2E2E4),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Inspection Fee",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontFamily: "OpenBold"
-                          ),
-                        ),
-                        const Text(
-                          "N 5,000",
-                          style: TextStyle(
-                            color: Color(0xff00BFFF),
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
+                    Divider(
+                      thickness: 1,
+                      color: Color(0xffE2E2E4),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Payment",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontFamily: "OpenBold"),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Wallet Charge",
+                                style: TextStyle(
+                                  color: Color(0xff25D366),
+                                  fontSize: 12,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                Iconsax.add,
+                                color: Colors.black,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      thickness: 1,
+                      color: Color(0xffE2E2E4),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Total",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontFamily: "OpenBold"),
+                          ),
+                          Text(
+                            "N 5,000",
+                            style: TextStyle(
+                              color: Color(0xff00BFFF),
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      isScrollControlled: true
-    );
+        isScrollControlled: true);
   }
 }
