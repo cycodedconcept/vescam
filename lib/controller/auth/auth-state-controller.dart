0db327912c5e2d055bd06a9f3fabacf6176c626e
tade-dev <akintadeseun816@gmail.com>
2024-08-28 01:43:41 +0100

    implemented geolocation

--- lib/controller/auth/auth-state-controller.dart
+++ lib/controller/auth/auth-state-controller.dart
@@ -1,4 +1,12 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:toastification/toastification.dart';
+import 'package:vescan/storage/local-storage.dart';
+
+import '../../routes/app/app-route-names.dart';
+import '../../services/auth/auth-services.dart';
 
 class AuthStateController extends GetxController {
   // INSTANT VARIABLES
@@ -68,6 +76,7 @@ class AuthStateController extends GetxController {
     _deviceId = value;
     update();
   }
+
   updateIsLoading(value) {
     _isLoading = value;
     update();
@@ -81,5 +90,147 @@ class AuthStateController extends GetxController {
   toggleShowPassword() {
     _showPassword = !_showPassword;
     update();
+  }
+
+  // API CALLS
+  Future loginUser() async {
+    updateIsLoading(true);
+
+    var data = {"email": _email, "password": _password};
+
+    var response = await AuthServices().loginUserService(data);
+    var responseData = response!.data;
+    log(responseData.toString());
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      updateIsLoading(false);
+
+      await LocalStorage().storeUserToken(responseData['token']);
+
+      toastification.show(
+        style: ToastificationStyle.fillColored,
+        type: ToastificationType.success,
+        title: const Text("Success"),
+        description: Text(responseData["message"]),
+        autoCloseDuration: const Duration(seconds: 3),
+      );
+
+      Get.offAllNamed(dashboard);
+    } else {
+      updateIsLoading(false);
+
+      Get.back();
+    }
+  }
+
+  Future registerUser() async {
+    updateIsLoading(true);
+
+    var data = {
+      "name": _name,
+      "email": _email,
+      "country": _country,
+      "phone_number": _phoneNumber,
+      "password": _password,
+      "password_confirmation": _confirmPassword,
+    };
+
+    log(data.toString());
+
+    var response = await AuthServices().registerUserService(data);
+    var responseData = response!.data;
+    log(responseData.toString());
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      updateIsLoading(false);
+
+      await LocalStorage().storeUserToken(responseData["token"]);
+
+      Get.offAllNamed(signUpVerify);
+    } else {
+      updateIsLoading(false);
+
+      toastification.show(
+        style: ToastificationStyle.fillColored,
+        type: ToastificationType.error,
+        title: const Text("Error"),
+        description: Text(responseData["message"]),
+        autoCloseDuration: const Duration(seconds: 3),
+      );
+
+      Get.back();
+    }
+  }
+
+  Future verifyOtp() async {
+    updateIsLoading(true);
+
+    var data = {
+      "email": _email,
+      "otp": _otpCode,
+    };
+
+    log(data.toString());
+
+    var response = await AuthServices().verifyEmailOtpService(data);
+    var responseData = response!.data;
+    log(responseData.toString());
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      updateIsLoading(false);
+
+      Get.offAndToNamed(login);
+    } else {
+      updateIsLoading(false);
+
+      toastification.show(
+        style: ToastificationStyle.fillColored,
+        type: ToastificationType.error,
+        title: const Text("Error"),
+        description: Text(responseData["message"]),
+        autoCloseDuration: const Duration(seconds: 3),
+      );
+    }
+  }
+
+  Future resetPassword() async {
+    updateIsLoading(true);
+
+    var data = {
+      "email": _email,
+      "password": _password,
+      "password_confirmation": _confirmPassword,
+    };
+
+    log(data.toString());
+
+    var response = await AuthServices().updatePasswordService(data);
+    var responseData = response!.data;
+    log(responseData.toString());
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      updateIsLoading(false);
+
+      // Get.offAndToNamed(login);
+    } else {
+      updateIsLoading(false);
+
+      toastification.show(
+        style: ToastificationStyle.fillColored,
+        type: ToastificationType.error,
+        title: const Text("Error"),
+        description: Text(responseData["message"]),
+        autoCloseDuration: const Duration(seconds: 3),
+      );
+    }
+  }
+
+  Future refreshToken() async {
+    var response = await AuthServices().refreshTokenService();
+    var responseData = response!.data;
+    log(responseData.toString());
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+    } else {}
   }
 }
--- lib/screens/splash/splash-screen.dart
+++ lib/screens/splash/splash-screen.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/route_manager.dart';
 import 'package:vescan/routes/app/app-route-names.dart';
+import 'package:vescan/storage/local-storage.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,24 +16,115 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  int _currentStep = 0;
+
+
+getLocation() async {
+  setState(() {
+    _currentStep = 20; // Update progress to 20%
+  });
+
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  setState(() {
+    _currentStep = 40; // Update progress to 40%
+  });
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  } else if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission != LocationPermission.whileInUse &&
+        permission != LocationPermission.always) {
+      return Future.error(
+          'Location permissions are denied (actual value: $permission).');
+    }
+  }
+
+  setState(() {
+    _currentStep = 60; // Update progress to 60%
+  });
+
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+  log('Latitude: ${position.latitude}, Longitude: ${position.longitude}');
+
+  setState(() {
+    _currentStep = 80; // Update progress to 80%
+  });
+
+  List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude, position.longitude);
+
+  String country = placemarks.first.country ?? '';
+  String state = placemarks.first.administrativeArea ?? '';
+  String address = placemarks.first.street ?? '';
+
+  Map<String, dynamic> locationData = {
+    "longitude": position.longitude,
+    "latitude": position.latitude,
+    "country": country,
+    "state": state,
+    "address": address,
+  };
+
+  String encodedData = jsonEncode(locationData);
+
+  await LocalStorage().storeLocationData(encodedData);
+
+  setState(() {
+    _currentStep = 100;
+  });
+
+  String authToken = await LocalStorage().fetchUserToken();
+
+  (authToken.isEmpty)
+      ? Get.toNamed(onboarding)
+      : Get.toNamed(login);
+}
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.toNamed(onboarding);
-    });
+    getLocation();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: const Color(0xff001F3F),
-        child: Center(
-          child: Image.asset("assets/images/logo-1.png"),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: const Color(0xff001F3F),
+            child: Center(
+              child: Image.asset("assets/images/logo-1.png"),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: CircularProgressIndicator(
+                  value: _currentStep.toDouble(),
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
